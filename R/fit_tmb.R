457df891e17199efab2265aa4dc59601a260ca4f
osymandius <bolistevens@gmail.com>
2020-09-04 12:52:37 +0100

    fit tmb

---
+++ R/fit_tmb.R
@@ -0,0 +1,22 @@
+#' Fit TMB model
+#'
+#' @export
+
+fit_sample_tmb <- function(data, par, random) {
+  obj <-  MakeADFun(data = data,
+                    parameters = par,
+                    DLL = "multi",
+                    random = random,
+                    hessian = FALSE)
+
+  f <- nlminb(obj$par, obj$fn, obj$gr)
+  f$par.fixed <- f$par
+  f$par.full <- obj$env$last.par
+
+  fit <- c(f, obj = list(obj))
+  fit$sdreport <- sdreport(fit$obj, fit$par)
+
+  class(fit) <- "naomi_fit"  # this is hacky...
+  fit <- sample_tmb(fit, random_only=FALSE)
+  return(fit)
+}
