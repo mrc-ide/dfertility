d43240b58bdfae8b7dda070326d72c3c2f904622
osymandius <bolistevens@gmail.com>
2020-09-21 13:25:19 +0100

    Remove dependency on Naomi
    Bring sample_tmb into dfertility

--- R/fit_tmb.R
+++ R/fit_tmb.R
@@ -1,6 +1,8 @@
 #' Fit TMB model
 #'
 #' @export
+#'
+#'
 
 fit_sample_tmb <- function(data, par, random) {
   obj <-  MakeADFun(data = data,
@@ -19,4 +21,51 @@ fit_sample_tmb <- function(data, par, random) {
   class(fit) <- "naomi_fit"  # this is hacky...
   fit <- sample_tmb(fit, random_only=FALSE)
   return(fit)
+}
+
+#' Sample TMB
+#'
+
+sample_tmb <- function (fit, nsample = 1000, rng_seed = NULL, random_only = TRUE,
+          verbose = FALSE)
+{
+  set.seed(rng_seed)
+  stopifnot(methods::is(fit, "naomi_fit"))
+  stopifnot(nsample > 1)
+  if (!random_only) {
+    if (verbose)
+      print("Calculating joint precision")
+    hess <- sdreport_joint_precision(fit$obj, fit$par.fixed)
+    if (verbose)
+      print("Inverting precision for joint covariance")
+    cov <- solve(hess)
+    if (verbose)
+      print("Drawing sample")
+    smp <- mvtnorm::rmvnorm(nsample, fit$par.full, cov)
+  }
+  else {
+    r <- fit$obj$env$random
+    par_f <- fit$par.full[-r]
+    par_r <- fit$par.full[r]
+    hess_r <- fit$obj$env$spHess(fit$par.full, random = TRUE)
+    smp_r <- rmvnorm_sparseprec(nsample, par_r, hess_r)
+    smp <- matrix(0, nsample, length(fit$par.full))
+    smp[, r] <- smp_r
+    smp[, -r] <- matrix(par_f, nsample, length(par_f), byrow = TRUE)
+    colnames(smp)[r] <- colnames(smp_r)
+    colnames(smp)[-r] <- names(par_f)
+  }
+  if (verbose)
+    print("Simulating outputs")
+  sim <- apply(smp, 1, fit$obj$report)
+  r <- fit$obj$report()
+  if (verbose)
+    print("Returning sample")
+  fit$sample <- Map(vapply, list(sim), "[[", lapply(lengths(r),
+                                                    numeric), names(r))
+  is_vector <- vapply(fit$sample, inherits, logical(1), "numeric")
+  fit$sample[is_vector] <- lapply(fit$sample[is_vector], matrix,
+                                  nrow = 1)
+  names(fit$sample) <- names(r)
+  fit
 }
